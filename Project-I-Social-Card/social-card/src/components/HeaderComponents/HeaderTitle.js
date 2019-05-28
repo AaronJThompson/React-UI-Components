5d01d83245d3ec1b0b1c9ffd70cd23dc85ce4629
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-28 12:57:14 +0100

    Add HeaderTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle(props) {
+    const { title, userHandle, date } = props;
+
+    return (
+        <div className="title">
+            {title}
+
+            <span className="post-info">
+                {`@${userHandle} • ${date}`}
+            </span>
+        </div>
+    );
+}
+
+export default HeaderTitle;
