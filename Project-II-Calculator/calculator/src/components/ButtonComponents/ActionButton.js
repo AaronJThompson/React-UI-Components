e171b7b7a30f6957ae5bba6342a63a0bf8b7338f
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-28 15:39:19 +0100

    Add NumberButton and ActionButton

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+function ActionButton(props) {
+    const { text } = props;
+
+    return (
+        <button className="action-button">{text}</button>
+    );
+}
+
+export default ActionButton;
--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+function NumberButton(props) {
+    const { text, buttonStyle } = props;
+
+    return (
+        <button className={buttonStyle}>{text}</button>
+    );
+}
+
+export default NumberButton;
