0722dae18d0aa47fd995d88d2d04d78e5cee3d51
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-28 13:15:33 +0100

    Add CardContent component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent(props) {
+    const { title, body, link } = props;
+
+    return (
+        <div className="card-content">
+            <h3>{title}</h3>
+            <p>{body}</p>
+            <span>{link}</span>
+        </div>
+    );
+}
+
+export default CardContent;
