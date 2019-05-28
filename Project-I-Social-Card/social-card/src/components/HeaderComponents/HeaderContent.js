3a2e26a2c348740a04b1a34fab56be6212aedd53
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-28 13:02:03 +0100

    Add HeaderContent component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContent.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import HeaderTitle from './HeaderTitle';
+
+function HeaderContent(props) {
+    const { content, postInfo} = props;
+
+    const {  title, userHandle, date } = postInfo;
+
+    return (
+        <div className="content">
+            {content}
+            <HeaderTitle title={title} userHandle={userHandle} date={date} />
+        </div>
+    );
+}
+
+export default HeaderContent;
