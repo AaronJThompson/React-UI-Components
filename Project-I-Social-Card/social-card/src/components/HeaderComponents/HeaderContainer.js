269093c1a557cfdec813361dd49d80f9328829ad
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-28 13:10:41 +0100

    Add HeaderContainer component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+import HeaderContent from './HeaderContent';
+
+function HeaderContainer(props) {
+    const { imageSrc, content, postInfo } = props;
+
+    return (
+        <div className="post-header">
+            <ImageThumbnail imageSrc={imageSrc} />
+            <HeaderContent content={content} postInfo={postInfo} />
+        </div>
+    )
+}
+
+export default HeaderContainer;
