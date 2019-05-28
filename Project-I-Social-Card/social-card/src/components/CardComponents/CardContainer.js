205475ea85fddf643630e80b1459384276a3fc68
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-28 13:35:42 +0100

    Add CardContainer component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+function CardContainer(props) {
+    const { banner, content} = props;
+
+    return (
+        <div className="card">
+            <CardBanner imageSrc={banner.imageSrc} />
+            <CardContent  title={content.title} body={content.body} link={content.link} />
+        </div>
+    );
+}
+
+export default CardContainer;
