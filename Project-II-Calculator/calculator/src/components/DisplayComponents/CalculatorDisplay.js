19d8a3d950fc3677cefc8a372bd7a89945af964b
AaronJThompson <aaronjthompson98@gmail.com>
2019-05-28 15:43:37 +0100

    Add CalculatorDisplay

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import './Display.css';
+
+function CalculatorDisplay(props) {
+    const { text } = props;
+
+    return (
+        <div className="display">
+            <span>
+                {text}
+            </span>
+        </div>
+    );
+}
+export default CalculatorDisplay;
